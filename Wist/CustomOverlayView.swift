c0bbe09c8da8df8319b3247eb99d4b0ab4e13c7b
bryanye97 <bryanye97@gmail.com>
2016-07-03 16:05:42 -0700

    Added CustomOverlayView

---
+++ Wist/CustomOverlayView.swift
@@ -0,0 +1,40 @@
+//
+//  CustomOverlayView.swift
+//  Wist
+//
+//  Created by Bryan Ye on 3/07/2016.
+//  Copyright © 2016 Bryan Ye. All rights reserved.
+//
+
+import UIKit
+import Koloda
+
+private let overlayRightImageName = "overlay_like"
+private let overlayLeftImageName = "overlay_skip"
+
+class CustomOverlayView: OverlayView {
+    
+    @IBOutlet lazy var overlayImageView: UIImageView! = {
+        [unowned self] in
+        
+        var imageView = UIImageView(frame: self.bounds)
+        self.addSubview(imageView)
+        
+        return imageView
+        }()
+    
+    override var overlayState: SwipeResultDirection?  {
+        didSet {
+            switch overlayState {
+            case .Left? :
+                overlayImageView.image = UIImage(named: overlayLeftImageName)
+            case .Right? :
+                overlayImageView.image = UIImage(named: overlayRightImageName)
+            default:
+                overlayImageView.image = nil
+            }
+            
+        }
+    }
+    
+}
